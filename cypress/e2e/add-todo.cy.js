6ebb906de78d2d0a1b34e484c55ed28ff55ea650
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-12-31 11:57:43 -0500

    initial

--- cypress/e2e/add-todo.cy.js
+++ cypress/e2e/add-todo.cy.js
@@ -2,4 +2,8 @@
 
 it('adds a todo', () => {
   cy.visit('/')
+  cy.get('.new-todo').type('item 1{enter}')
+  cy.get('li.todo').should('have.length', 1)
+  cy.get('.new-todo').type('item 2{enter}')
+  cy.get('li.todo').should('have.length', 2)
 })
