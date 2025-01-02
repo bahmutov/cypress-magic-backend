d79b0ec71e7acdfc1dd4d2153cb0dfd2ed9d77a0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-01-02 15:10:28 -0500

    testing delete

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,100 +1,17 @@
 /// <reference types="cypress" />
 // @ts-check
 
-const label = 'cypress-magic-backend'
-// Cypress.env('magic_backend_mode', 'recording')
-// Cypress.env('magic_backend_mode', 'playback')
-
-const apiCallsInThisTest = []
+// https://github.com/bahmutov/cypress-cdp
+import 'cypress-cdp'
+import './plugin'
 
 beforeEach(() => {
-  apiCallsInThisTest.length = 0
-
   const mode = Cypress.env('magic_backend_mode')
-  switch (mode) {
-    case 'recording':
-      cy.log(`**${label}** Recording mode`)
-      cy.intercept(
-        {
-          method: '*',
-          path: '/todos',
-        },
-        (req) => {
-          req.continue((res) => {
-            apiCallsInThisTest.push({
-              method: req.method,
-              url: req.url,
-              request: req.body,
-              response: res.body,
-            })
-          })
-        },
-      )
-      break
-    case 'playback':
-      cy.log(`**${label}** Playback mode`)
-      const filename = formTestRecordingFilename(
-        Cypress.spec,
-        Cypress.currentTest,
-      )
-      // for now assuming the file exists
-      cy.readFile(filename).then((apiCalls) => {
-        let apiCallIndex = 0
-        cy.intercept(
-          {
-            method: '*',
-            path: '/todos',
-          },
-          (req) => {
-            const apiCall = apiCalls[apiCallIndex]
-            if (!apiCall) {
-              throw new Error(
-                `Ran out of recorded API calls at index ${apiCallIndex}`,
-              )
-            }
-            apiCallIndex += 1
-            if (req.method !== apiCall.method) {
-              throw new Error(
-                `Expected method ${apiCall.method} but got ${req.method}`,
-              )
-            }
-            if (req.url !== apiCall.url) {
-              throw new Error(
-                `Expected URL ${apiCall.url} but got ${req.url}`,
-              )
-            }
-            // todo: check the request body
-            req.reply(apiCall.response)
-          },
-        )
-      })
-      break
-  }
-})
-
-function formTestRecordingFilename(currentSpec, currentTest) {
-  const specName = Cypress.spec.relative
-  const title = Cypress.currentTest.titlePath
-    .join('_')
-    .replaceAll(' ', '_')
-  return `cypress/magic-backend/${specName}_${title}_api_calls.json`
-}
-
-afterEach(() => {
-  const mode = Cypress.env('magic_backend_mode')
-  switch (mode) {
-    case 'recording':
-      const specName = Cypress.spec.relative
-      const title = Cypress.currentTest.titlePath.join('_')
-      cy.log(
-        `Recording ${apiCallsInThisTest.length} API calls for ${specName} "${title}"`,
-      )
-      const filename = formTestRecordingFilename(
-        Cypress.spec,
-        Cypress.currentTest,
-      )
-      cy.writeFile(filename, apiCallsInThisTest)
-      break
-    // for the playback mode we could check that all API calls were used
+  if (mode === 'recording') {
+    // use the cy.CDP command to disable the network caching
+    // https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-setCacheDisabled
+    cy.CDP('Network.setCacheDisabled', {
+      cacheDisabled: true,
+    })
   }
 })
---
+++ cypress/support/plugin.js
@@ -0,0 +1,119 @@
+/// <reference types="cypress" />
+// @ts-check
+
+const label = 'cypress-magic-backend'
+// Cypress.env('magic_backend_mode', 'recording')
+// Cypress.env('magic_backend_mode', 'playback')
+
+/**
+ * We want the user to not worry about the exact precise keywords
+ * used to set the mode to "recording" or "playback".
+ * Thus we allow the user to say the mode to "record" or "play"
+ * and simply normalize the mode to "recording" or "playback".
+ */
+function normalizeBackendMode() {
+  const mode = Cypress.env('magic_backend_mode')
+  switch (mode) {
+    case 'record':
+    case 'recording':
+      Cypress.env('magic_backend_mode', 'recording')
+      break
+    case 'play':
+    case 'playback':
+      Cypress.env('magic_backend_mode', 'playback')
+      break
+  }
+}
+
+const apiCallsInThisTest = []
+
+beforeEach(() => {
+  normalizeBackendMode()
+
+  // which calls to intercept?
+  const apiCallsToIntercept = {
+    method: '*',
+    resourceType: 'xhr',
+  }
+
+  apiCallsInThisTest.length = 0
+
+  const mode = Cypress.env('magic_backend_mode')
+  switch (mode) {
+    case 'record':
+    case 'recording':
+      cy.log(`**${label}** Recording mode`)
+      cy.intercept(apiCallsToIntercept, (req) => {
+        req.continue((res) => {
+          apiCallsInThisTest.push({
+            method: req.method,
+            url: req.url,
+            request: req.body,
+            response: res.body,
+          })
+        })
+      })
+      break
+    case 'play':
+    case 'playback':
+      cy.log(`**${label}** Playback mode`)
+      const filename = formTestRecordingFilename(
+        Cypress.spec,
+        Cypress.currentTest,
+      )
+      // for now assuming the file exists
+      cy.readFile(filename).then((apiCalls) => {
+        let apiCallIndex = 0
+        cy.intercept(apiCallsToIntercept, (req) => {
+          const apiCall = apiCalls[apiCallIndex]
+          if (!apiCall) {
+            throw new Error(
+              `Ran out of recorded API calls at index ${apiCallIndex}`,
+            )
+          }
+          apiCallIndex += 1
+          if (req.method !== apiCall.method) {
+            throw new Error(
+              `Expected method ${apiCall.method} but got ${req.method}`,
+            )
+          }
+          if (req.url !== apiCall.url) {
+            throw new Error(
+              `Expected URL ${apiCall.url} but got ${req.url}`,
+            )
+          }
+          // todo: check the request body
+          req.reply(apiCall.response)
+        })
+      })
+      break
+  }
+})
+
+function formTestRecordingFilename(currentSpec, currentTest) {
+  const specName = Cypress.spec.relative
+  const title = Cypress.currentTest.titlePath
+    .join('_')
+    .replaceAll(' ', '_')
+  return `cypress/magic-backend/${specName}_${title}_api_calls.json`
+}
+
+afterEach(() => {
+  const mode = Cypress.env('magic_backend_mode')
+  switch (mode) {
+    case 'record':
+    case 'recording':
+      const specName = Cypress.spec.relative
+      const title = Cypress.currentTest.titlePath.join('_')
+      cy.log(
+        `Recording ${apiCallsInThisTest.length} API calls for ${specName} "${title}"`,
+      )
+      const filename = formTestRecordingFilename(
+        Cypress.spec,
+        Cypress.currentTest,
+      )
+      cy.writeFile(filename, apiCallsInThisTest)
+      break
+    // for the playback mode we could check that all API calls were used
+  }
+})
