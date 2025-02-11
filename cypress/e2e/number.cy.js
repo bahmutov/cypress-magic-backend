330eca92a10d92d660b054ffbec46fa4182800d8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-11 18:38:51 -0500

    Add random endpoint (#49)
    * adding number spec
    
    * making sure the test works
    
    * fix the next call
    
    * handle playback only
    
    * add one more line to the array config for the number spec
    
    * rename a CI job

---
+++ cypress/e2e/number.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+it('reflects the sent number', () => {
+  cy.visit('/number.html')
+  const random = Cypress._.random(-3, 30)
+  cy.get('#num').should('have.value', 0).clear().type(random)
+  cy.contains('button', 'Send number').click()
+
+  const mode = Cypress.env('magic_backend_mode')
+  if (mode === 'playback' || mode === 'playback-only') {
+    // the response is mocked and we cannot access it easily
+    // but it won't be the random number we sent
+    cy.contains('#answer', /server response: \d+/)
+  } else {
+    cy.get('#answer').should(
+      'have.text',
+      `server response: ${random}`,
+    )
+  }
+})
